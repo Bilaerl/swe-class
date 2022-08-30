943d7363dd3ec42c429f87b865ecbac44d69a510
Abubakar Beni <abubakarbeni12@gmail.com>
2022-08-30 12:44:57 +0100

    . chooses two random small letters and add them to the random_password list

--- random_password_generator.py
+++ random_password_generator.py
@@ -3,3 +3,7 @@ import random
 random_password = []
 lowercase= ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 uppercase = [lowercase.upper() for lowercase in lowercase]
+
+def small_letters():
+    random_password+=random.choices(lowercase, k=2)
+
