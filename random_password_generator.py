ecdaab67377d934fdd925a437023fdb3ed03a5a9
Abubakar Beni <abubakarbeni12@gmail.com>
2022-08-30 12:56:23 +0100

    . shuffles random_password list and returns a string

--- random_password_generator.py
+++ random_password_generator.py
@@ -7,3 +7,17 @@ uppercase = [lowercase.upper() for lowercase in lowercase]
 def small_letters():
     random_password+=random.choices(lowercase, k=2)
 
+def capital_letters():
+     random_password+=random.choices(uppercase, k=2)
+
+def num_chars():
+    random_password+=random.randint(0,9,k=2)
+
+def special_chars()
+    random_password+=random.choices(special_characters,k=2)
+
+def random_password():
+    random.shuffle(random_password)
+    ''.join(random_password)
+    return random_password
+    
