09ace530c4e5fec60260f47d687fe226159c3052
Abubakar Beni <abubakarbeni12@gmail.com>
2022-08-30 12:40:19 +0100

    . random module imported
    variables assigned

--- random_password_generator.py
+++ random_password_generator.py
@@ -1,19 +1,5 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Aug 30 11:49:57 2022
-
-@author: ABUBAKAR BENII
-"""
-
 import random
 
 random_password = []
 lowercase= ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 uppercase = [lowercase.upper() for lowercase in lowercase]
-special_characters=["!","@","#","$","%","^","&","*","(",")","+","?","/",".",">",",","<","|",";",":","]",":",",","}","[","{","}"]
-
-def small_letters():
-    global random_password
-    random_password+=random.choices(lowercase, k=2)
-small_letters()
-print(random_password)
