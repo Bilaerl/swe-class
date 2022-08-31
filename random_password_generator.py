06f6c5377c93f704762056c0feffe61b051aa72d
vin_caveer <ahmadkabir380@gmail.com>
2022-08-31 12:26:34 +0100

    the review of the code

--- random_password_generator.py
+++ random_password_generator.py
@@ -5,12 +5,15 @@ nums = ["0",'1','2','3','4','5','7','8','9']
 
 import random
 
+"""" this work for making random password
 
+it's give two diff kind of random password
+"""
 
 
 def capital_selection():
 
-    
+    # this select two alp from capital_alp and return them,
     capital = 0
 
     while capital < 10:
@@ -18,7 +21,7 @@ def capital_selection():
         
         capital0 = random.sample(capital_alp,2)
         capital += 1
-        # print(capital0)
+        
         if capital >= 9:
             return capital0
         
@@ -29,15 +32,13 @@ my_capital = capital_selection()
 
 def small_selection():
 
-    
+    # this select two alp from small_alp and return them,
     small = 0
 
     while small < 9: 
         small0 = random.sample(small_alp,2)
         small += 1
-        # b = float(capital0)
         
-        # (print(small0))
         if small >= 9:
 
             return small0
@@ -47,15 +48,13 @@ my_small = small_selection()
 
 def character_selection():
 
-    
+    # this select two charaters from special_character and return them,
     character = 0
 
     while character < 9: 
         character0 = random.sample(special_characters,2)
         character += 1
-        # b = float(capital0)
         
-        # (print(character0))
         if character >= 9:
 
             return character0
@@ -66,56 +65,47 @@ my_character = character_selection()
 
 def nums_selection():
 
-    
+    # this select two numbers from num and return them,
     num = 0
 
     while num < 9: 
         num0 = random.sample(nums,2)
         num += 1
-        # b = float(capital0)
-        
-        # (print(num0))
+       
         if num >= 9:
 
             return num0
 
         
 my_num = nums_selection()
-# print(my_capital + my_small + my_character + my_num)
 
 
 def first_main():
     password = 0
     while password < 9:
-        # main_password = random.sample(my_capital + my_small + my_character + my_num,8)
-        # my_capital1 =capital_selection()
-        # my_small1 = small_selection()
-        # my_character1 = character_selection()
-        # my_num1 = nums_selection()
-        # # print(capital_selection())
+        
         main_password = random.sample(my_capital + my_small + my_character + my_num,8)
         password += 1
-        # last_password = my_capital1 + my_small1 + my_character1 + my_num1
+        
         last_password = main_password
         print("".join(last_password))
 
         
-# first_main()
+first_main()
 
 
 def second_main():
     password = 0
     while password < 9:
-        # main_password = random.sample(my_capital + my_small + my_character + my_num,8)
+        
         my_capital1 =capital_selection()
         my_small1 = small_selection()
         my_character1 = character_selection()
         my_num1 = nums_selection()
-        # print(capital_selection())
-        # main_password = random.sample(my_capital + my_small + my_character + my_num,8)
+        
         password += 1
         last_password = my_capital1 + my_small1 + my_character1 + my_num1
-        # last_password = main_password
+        
         print("".join(last_password))
 
         
