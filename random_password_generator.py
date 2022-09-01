1d8ef259990c51310474f839f1e7bb69c2f1f681
Abubakar Beni <abubakarbeni12@gmail.com>
2022-09-01 14:08:27 +0100

    .

--- random_password_generator.py
+++ random_password_generator.py
@@ -1,63 +1,46 @@
-"""This program generates a string containing eight random characters, two each of 
-capital letters, small letters, numerical characters and special character,
-using the python built-in random module
-""""
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 31 20:34:24 2022
 
+@author: ABUBAKAR BENII
+"""
 
 import random
 
-random_password = []
-lowercase= ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-uppercase = [lowercase.upper() for lowercase in lowercase]
-special_characters = ["!","@","#","$","%","^","&","*","(",")","+","?","/",".",">",",","<","|",";",":","]",":",",","}","[","{","}"]
+randompassword=[]
+lower =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+upper=[i.upper() for i in lower]
+special_xters = ["!","@","#","$","%","^","&","*","(",")","+","?","/",".",">",",","<","|",";",":","]",":",",","}","[","{","}"]
 
-def small_letters():
+def small():
+    global randompassword
+    randompassword+=random.choices(lower,k=2)
     
-    """randomly picks two elements from the lowercase list 
-        and adds them to the random_password list"""
-        
-    random_password+=random.choices(lowercase, k=2)
-
-
-def capital_letters():
-    """randomly picks two elements from the uppercase list 
-        and adds them to the random_password list"""
-        
-     random_password+=random.choices(uppercase, k=2)
-
-
-def num_chars():
-    """picks two random integers from 0 to 10 
-        and adds them to the random_password list"""
-        
-    random_password+=random.choices(range(10),k=2)
-
-
-def special_chars():
-    """randomly picks two elements from the special_characters list 
-        and adds them to the random_password list"""
-        
-    random_password+=random.choices(special_characters,k=2)
-
-
+def capital():
+    global randompassword
+    randompassword+=random.choices(upper,k=2)
+    
+def special():
+    global randompassword
+    randompassword+=random.choices(special_xters,k=2)
+    
+def numerical():
+    global randompassword
+    randompassword+=random.choices(range(10),k=2)
+    
 def random_password():
-    
-    """shuffles the random_password list
-    and forms a string using the join() method
-    returns the formed string
-    """
-    
-    random.shuffle(random_password)
-    ''.join(str(random_password) for random_password in random_password)
-    return random_password
+    global randompassword
+    random.shuffle(randompassword)
+    randompassword= ''.join(str(i) for i in randompassword)
+    print(randompassword)
 
 def main():
-    """"calls all the functions above
-    """
-    small_letters()
-    capital_letters()
-    num_chars()
-    special_chars()
+    small()
+    capital()
+    special()
+    numerical()
     random_password()
     
+    
 main()
+
